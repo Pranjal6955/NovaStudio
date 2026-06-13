96a989ca567b1024320c3b9561463d2e46bcc03e
Pranjal6955 <negipranjal555@gmail.com>
2026-06-13 21:35:22 +0530

    Updated Server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,9 @@
 import express from "express";
 import dotenv from "dotenv"
-import { serviceRouter } from "./routes/service.routes.js";
-
-
 dotenv.config()
-
+import { serviceRouter } from "./routes/service.routes.js";
+import { projectRoute } from "./routes/project.routes.js";
+import { statsRoute } from "./routes/stats.routes.js";
 
 const app = express();
 const PORT = process.env.PORT
@@ -14,6 +13,8 @@ app.get("/",(req,res)=>{
     return res.status(200).json({message:"Welcome to NovaStudio : Backend is Live"})
 })
 app.use("/api/service",serviceRouter)
+app.use("/api/project",projectRoute)
+app.use("/api/stats",statsRoute)
 
 app.listen(PORT,()=>{
     console.log(`Server is running on http://localhost:${PORT}`)
